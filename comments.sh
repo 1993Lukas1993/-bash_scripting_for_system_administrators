0a9cbdc7dd6992db20618e1ceef039820a88b0bf
1993Lukas1993 <lukasbubeliny@gmail.com>
2023-03-07 15:26:35 +0000

    added comments.sh

---
+++ comments.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This line is intended to be used as a general description of the script
+# and anything that it does
+
+
+
+clear   # clears the screen
+
+#MYUSERNAME="Terry"     # The username for this application
+MYUSERNAME="Don"	# New username added later
+
+echo "We are using the default user called: $MYUSERNAME"    # Display to the console
+
+
+DATETIMESTAMP=`date`
+
+echo "This is when the script was run: $DATETIMESTAMP"   # This is the timestamp of run"
