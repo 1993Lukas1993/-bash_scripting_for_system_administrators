33312562f6e7057acd5f372090102d6a20ec491f
1993Lukas1993 <lukasbubeliny@gmail.com>
2023-03-07 14:38:05 +0000

    added env.sh

---
+++ env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+clear
+
+echo "This script will give us environment information"
+echo "=================================================="
+echo "Hello $USER"
+echo ""
+echo "Your Home Directory is:" $HOME
+echo ""
+echo "Your History file will ignore:" $HISTCONTROL
+echo ""
+echo "Your terminal sesson type is:" $TERM
+echo ""
